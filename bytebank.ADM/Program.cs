30342bbab716ff0c8dc85f368dcd1b0db27ca322
matheus <drchaos820@gmail.com>
2022-11-07 18:56:30 -0300

    Criação de codigo duplicado ( Não é legal fazer isso )

--- bytebank.ADM/Program.cs
+++ bytebank.ADM/Program.cs
@@ -1,10 +1,26 @@
 ﻿
 using bytebank.ADM.Funcionarios;
+using bytebank.ADM.Funcionarios.Utilitario;
 
-Funcionario pedro = new Funcionario(0);
+Funcionario pedro = new Funcionario(1);
 pedro.Nome = "Matheus Silveiro";
 pedro.Cpf = "12345690";
 pedro.Salario = 2000;
 
 Console.WriteLine(pedro.Nome);
 Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberto = new Diretor();
+roberto.Nome = "Roberto Inacio";
+roberto.Cpf = "1231415";
+roberto.Salario = 5000;
+
+Console.WriteLine(roberto.Nome);
+Console.WriteLine(roberto.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberto);
+
+Console.WriteLine($"Total de bonificações: {gerenciador.TotalDeBonificacao}");
