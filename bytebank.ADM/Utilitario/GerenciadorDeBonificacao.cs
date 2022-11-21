58eec3879e9bd2c3bc4072350c3f15870fe48a97
matheus <drchaos820@gmail.com>
2022-11-21 18:48:21 -0300

    Ajuste de versão para dotnet 7.0

---
+++ bytebank.ADM/Utilitario/GerenciadorDeBonificacao.cs
@@ -0,0 +1,14 @@
+﻿using bytebank.ADM.Funcionarios;
+
+namespace bytebank.ADM.Utilitario
+{
+    public class GerenciadorDeBonificacao
+    {
+        public double TotalDeBonficacao { get; private set; }
+
+        public void Registrar(Funcionario funcionario)
+        {
+            this.TotalDeBonficacao += funcionario.GetBonificacao();
+        }
+    }
+}
